1e2373149f7af1d49b6a81532630945d1d5bb838
Deepak Keshri <104979465+unodeepak@users.noreply.github.com>
2024-02-16 18:50:06 +0530

    Time: 52 ms (69.01%), Space: 48.9 MB (28.02%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -4,19 +4,5 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-  // let num = 1;
-    
-  // for (let i = 1; i <= Math.abs(n); i++) {
-  //     if (n > 0) {
-  //         num = num * x;
-  //     } else {
-  //         num = x * 0.5;
-  //         x = num
-  //     }
-  //  }
-    // if (n > 0) {
-    x = Math.pow(x,n);
-    // } 
-
-   return x;
+  return Math.pow(x,n);
 };
