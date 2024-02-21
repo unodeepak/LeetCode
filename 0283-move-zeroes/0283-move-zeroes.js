53e93d2a663972a76169ab5b3fcb9081badf6374
Deepak Keshri <104979465+unodeepak@users.noreply.github.com>
2024-02-21 22:50:30 +0530

    Time: 82 ms (41.48%), Space: 54.5 MB (21.33%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -4,26 +4,12 @@
  */
 var moveZeroes = function(nums) {
     let len = nums.length;
-    let c = 0;
+    let ind = 0;
            
-    for (let i = 0; i < len; i++) {        
-        if (nums[i] == 0) {
-            let ind = i;
-            
-            while (len - 1 > ind) {
-                let temp = nums[ind];
-                nums[ind] = nums[ind + 1];
-                nums[ind + 1] = temp;
-                
-                ind++;   
-            }
-            if (nums[i] == 0) {
-                i--;
-                c++;
-            }
-        }
-        if (c == len) {
-            break;
+    for (let i = 0; i < len; i++) { 
+        if (nums[i] !== 0) {
+            [nums[i], nums[ind]] = [nums[ind], nums[i]];
+            ind++;
         }
     }
 };
