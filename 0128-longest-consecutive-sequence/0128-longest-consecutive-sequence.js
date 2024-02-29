25e19c47eaa93730bc0061c7272d20d3e07b79f0
Deepak Keshri <104979465+unodeepak@users.noreply.github.com>
2024-02-29 19:20:47 +0530

    Time: 101 ms (76.00%), Space: 72.5 MB (47.42%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    const numSet = new Set(nums);
+    let longestStreak = 1;
+
+    for (let num of numSet) {
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+
+            while (numSet.has(currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+
+  return nums.length == 0 ? 0 : longestStreak;
+};
