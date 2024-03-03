7346388d6d92cb0b4d32ce8089cfb158c801f248
Deepak Keshri <104979465+unodeepak@users.noreply.github.com>
2024-03-03 14:55:10 +0530

    Time: 131 ms (30.49%), Space: 67.8 MB (68.48%) - LeetHub

--- 2631-group-by/2631-group-by.js
+++ 2631-group-by/2631-group-by.js
@@ -3,21 +3,18 @@
  * @return {Object}
  */
 Array.prototype.groupBy = function(fn) {
-  let arr = [];
-  let arr1 = {};
-  let c = 1;
+  let arr = {};
   
   for (let item of this) {
     let key = fn(item);
     
-    if (arr1[key]) {
-      arr1[key].push(item);
+    if (arr[key]) {
+      arr[key].push(item);
     } else {
-      arr.push(key);
-      arr1[key] = [item];
+      arr[key] = [item];
     }
   }
-  return arr1;
+  return arr;
 };
 
 /**
