b33b80152a99156a02b915f2892c3f977b8d60c4
Deepak Keshri <104979465+unodeepak@users.noreply.github.com>
2024-03-11 12:23:34 +0530

    Time: 2272 ms (7.98%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,10 +1,1 @@
-# Write your MySQL query statement below
-# delete from person where email 
-
-# DELETE FROM person WHERE id NOT IN (SELECT MAX(id) AS MaxRecordID FROM person GROUP BY email);
-# WITH CTE([email], [id], DuplicateCount) AS (SELECT [email], [id], ROW_NUMBER() OVER(PARTITION BY [id], [email], ORDER BY ID) AS DuplicateCount FROM person DELETE FROM CTE WHERE DuplicateCount > 1;
-
-DELETE a1
-FROM person a1, person a2
-WHERE a1.email = a2.email
-  AND a1.id > a2.id;
+delete p1 from person p1, person p2 where p1.email = p2.email and p1.id > p2.id;
