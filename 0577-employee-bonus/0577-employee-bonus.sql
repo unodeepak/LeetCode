693bea8bc4f56f1f69003e7da7f6ce06d9590a02
Deepak Keshri <104979465+unodeepak@users.noreply.github.com>
2024-03-13 18:41:14 +0530

    Time: 2346 ms (14.84%), Space: 0B (100.00%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,1 +1,2 @@
-select e.name as name, b.bonus as bonus from employee e left join Bonus b on e.empId = b.empId Where b.bonus < 1000 OR b.bonus is null
+# SQL
+select e.name, b.bonus as bonus from employee e left join Bonus b on e.empId = b.empId Where b.bonus < 1000 OR b.bonus is null
